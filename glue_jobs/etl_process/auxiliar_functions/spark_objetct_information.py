94eba714dbf8e1d9c0e4a6bbb09c20efaaefcfe2
plagueis <samuel347@gmail.com>
2025-11-04 21:59:04 -0300

    add: docker file and ETL Spark lib

---
+++ glue_jobs/etl_process/auxiliar_functions/spark_objetct_information.py
@@ -0,0 +1,9 @@
+from etl_process.parameters import DB_PARAMETERS, S3_PARAMETERS
+
+class SparkEtlParametes:
+    def __init__(self, connector):
+        self.connector = connector
+        self.db_properties = DB_PARAMETERS.get("db_properties", None)
+        self.db_url = DB_PARAMETERS.get("db_url", None)
+        self.s3_path_root = S3_PARAMETERS.get("s3_path_root", None)
+        self.s3_path_target = S3_PARAMETERS.get("s3_path_target", None)
