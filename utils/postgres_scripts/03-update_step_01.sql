c5db9006827ae186223d3e544bfe0d937affe534
plagueis <samuel347@gmail.com>
2025-11-04 22:01:38 -0300

    add: license and SQL

---
+++ utils/postgres_scripts/03-update_step_01.sql
@@ -0,0 +1,350 @@
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 1;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 3;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 4;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 5;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 7;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 8;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 11;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 12;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 16;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 17;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 19;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 22;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 23;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 28;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 30;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 31;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 38;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 40;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 41;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 42;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 43;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 45;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 46;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 48;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 52;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 53;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 56;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 59;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 60;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 64;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 65;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 66;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 67;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 68;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 73;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 76;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 81;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 82;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 83;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 86;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 87;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 90;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 94;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 96;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 98;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 99;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 100;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 101;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 104;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 105;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 107;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 109;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 110;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 111;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 112;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 115;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 116;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 124;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 125;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 126;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 128;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 130;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 134;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 135;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 136;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 137;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 139;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 143;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 144;
+
+
+    UPDATE public.customers
+    SET last_updated_at = NOW()
+        WHERE customer_id = 150;
+
